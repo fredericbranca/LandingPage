d1c4ae5e4110ce310d009df10c05a7bef656153d
Frederic Branca <frederic.branca@gmail.com>
2023-04-25 14:06:13 +0200

    value de input dans update pricing

--- src/update_pricing.php
+++ src/update_pricing.php
@@ -20,61 +20,64 @@ require('db-functions.php'); ?>
 <body>
 
     <section class="container">
-
-        <div class="pricing-u">
-            <div class="text-pricing">
-                <h3>Update Pricing</h3>
-            </div>
-
-            <div class="cards-pricing">
-
-    <?php
-
-    foreach ($pricings as $pricing) { ?>
-
-                <div class="card-u">  
-                    <div class="card-u-center">
-                        <div>
-                            <label for="name">Name</label>
-                            <input type="text" id="name" name="name" value="<?= $pricing['name']?>" required>
-                        </div>
-                        <div>
-                            <label for="name">Price</label>
-                            <input type="number" id="price" name="price" value="<?= $pricing['price']?>" required>
-                        </div>
-                        <div>
-                            <label for="name">Sale</label>
-                            <input type="number" id="sale" name="sale" value="<?= $pricing['sale'] ?>" required>
-                        </div>
-                        <div>
-                            <label for="name">Bandwidth</label>
-                            <input type="number" id="bandwidth" name="bandwidth" value="<?= $pricing['bandwidth'] ?>" required>
-                        </div>
-                        <div>
-                            <label for="name">OnlineSpace</label>
-                            <input type="number" id="onlinespace" name="onlinespace" value="<?= $pricing['online_space'] ?>" required>
-                        </div>
-                        <div>
-                            <label for="name">Support</label>
-                            <input type="text" id="support" name="support" value="<?= $pricing['support'] ?>" required>
-                        </div>
-                        <div>
-                            <label for="name">Domain</label>
-                            <input type="text" id="domain" name="domain" value="<?= $pricing['domain'] ?>" required>
-                        </div>
-                        <div>
-                            <label for="name">Hidden Fees</label>
-                            <input type="text" id="hiddenfees" name="hiddenfees" value="<?= $pricing['hidden_fees'] ?>" required>
-                        </div>
-                    </div>
-                    <button>Update</button>
+        <form action="" methode="">
+            <div class="pricing-u">
+                <div class="text-pricing">
+                    <h3>Update Pricing</h3>
                 </div>
 
-    <?php 
-    } ?>
+                <div class="cards-pricing">
 
+        <?php
+
+        foreach ($pricings as $pricing) { ?>
+
+                    <div class="card-u">  
+                        <div class="card-u-center">
+                            <div>
+                                <label for="name">Name</label>
+                                <input type="text" id="name" name="name" value="<?= $pricing['name']?>" required>
+                            </div>
+                            <div>
+                                <label for="name">Price</label>
+                                <input type="number" min="0" id="price" name="price" value="<?= $pricing['price']?>" required>
+                            </div>
+                            <div>
+                                <label for="name">Sale</label>
+                                <input type="number" min="0" id="sale" name="sale" value="<?= $pricing['sale'] ?>" required>
+                            </div>
+                            <div>
+                                <label for="name">Bandwidth</label>
+                                <input type="number" min="0" id="bandwidth" name="bandwidth" value="<?= $pricing['bandwidth'] ?>" required>
+                            </div>
+                            <div>
+                                <label for="name">OnlineSpace</label>
+                                <input type="number" min="0" id="onlinespace" name="onlinespace" value="<?= $pricing['online_space'] ?>" required>
+                            </div>
+                            <div>
+                                <label for="name">Support</label>
+                                <input type="number" min="0" max="1" step="1" id="support" name="support" value="<?= $pricing['support'] ?>" required>
+                            </div>
+                            <div>
+                                <label for="name">Domain</label>
+                                <input type="text" id="domain" name="domain" value="<?= $pricing['domain'] ?>" required>
+                            </div>
+                            <div>
+                                <label for="name">Hidden Fees</label>
+                                <input type="number" min="0" max="1" step="1" id="hiddenfees" name="hiddenfees" value="<?= $pricing['hidden_fees'] ?>" required>
+                            </div>
+                        </div>
+                        <div class="button">
+                            <input type="submit" Value="Update">
+                        </div>
+                    </div>
+
+        <?php 
+        } ?>
+
+                </div>
             </div>
-        </div>
+        </form>
     </section>
 
 </body>
