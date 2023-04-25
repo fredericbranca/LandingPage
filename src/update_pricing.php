abdd9f06f0f85659d10af291712b84c2f7fd77bd
Frederic Branca <frederic.branca@gmail.com>
2023-04-25 16:59:18 +0200

    correction label for=

--- src/update_pricing.php
+++ src/update_pricing.php
@@ -1,6 +1,8 @@
 <?php
 
-require('db-functions.php'); ?>
+require('db-functions.php'); 
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -20,18 +22,18 @@ require('db-functions.php'); ?>
 <body>
 
     <section class="container">
-        <form action="" methode="">
-            <div class="pricing-u">
-                <div class="text-pricing">
-                    <h3>Update Pricing</h3>
-                </div>
+        <div class="pricing-u">
+            <div class="text-pricing">
+                <h3>Update Pricing</h3>
+            </div>
+            
+            <div class="cards-pricing">
+                
+                <?php
 
-                <div class="cards-pricing">
+foreach (getPricings() as $pricing) { ?>
 
-        <?php
-
-        foreach ($pricings as $pricing) { ?>
-
+                <form action="<?php setPricing(); ?>" method="POST" enctype="multipart/form-data">
                     <div class="card-u">  
                         <div class="card-u-center">
                             <div>
@@ -39,31 +41,31 @@ require('db-functions.php'); ?>
                                 <input type="text" id="name" name="name" value="<?= $pricing['name']?>" required>
                             </div>
                             <div>
-                                <label for="name">Price</label>
+                                <label for="price">Price</label>
                                 <input type="number" min="0" id="price" name="price" value="<?= $pricing['price']?>" required>
                             </div>
                             <div>
-                                <label for="name">Sale</label>
+                                <label for="sale">Sale</label>
                                 <input type="number" min="0" id="sale" name="sale" value="<?= $pricing['sale'] ?>" required>
                             </div>
                             <div>
-                                <label for="name">Bandwidth</label>
+                                <label for="bandwidth">Bandwidth</label>
                                 <input type="number" min="0" id="bandwidth" name="bandwidth" value="<?= $pricing['bandwidth'] ?>" required>
                             </div>
                             <div>
-                                <label for="name">OnlineSpace</label>
-                                <input type="number" min="0" id="onlinespace" name="onlinespace" value="<?= $pricing['online_space'] ?>" required>
+                                <label for="onlinespace">OnlineSpace</label>
+                                <input type="number" min="0,00" id="onlinespace" name="onlinespace" value="<?= $pricing['online_space'] ?>" required>
                             </div>
                             <div>
-                                <label for="name">Support</label>
+                                <label for="support">Support</label>
                                 <input type="number" min="0" max="1" step="1" id="support" name="support" value="<?= $pricing['support'] ?>" required>
                             </div>
                             <div>
-                                <label for="name">Domain</label>
+                                <label for="domain">Domain</label>
                                 <input type="text" id="domain" name="domain" value="<?= $pricing['domain'] ?>" required>
                             </div>
                             <div>
-                                <label for="name">Hidden Fees</label>
+                                <label for="hiddenfees">Hidden Fees</label>
                                 <input type="number" min="0" max="1" step="1" id="hiddenfees" name="hiddenfees" value="<?= $pricing['hidden_fees'] ?>" required>
                             </div>
                         </div>
@@ -71,13 +73,12 @@ require('db-functions.php'); ?>
                             <input type="submit" Value="Update">
                         </div>
                     </div>
-
+                </form>
         <?php 
         } ?>
 
                 </div>
             </div>
-        </form>
     </section>
 
 </body>
