d89b2e1015a0f294b0668c87a74ce78dfc9d3f83
Frederic Branca <frederic.branca@gmail.com>
2023-04-26 16:47:21 +0200

    update db

--- landingpage.sql
+++ landingpage.sql
@@ -19,6 +19,21 @@
 CREATE DATABASE IF NOT EXISTS `landingpage` /*!40100 DEFAULT CHARACTER SET latin1 */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `landingpage`;
 
+-- Listage de la structure de table landingpage. email
+CREATE TABLE IF NOT EXISTS `email` (
+  `id_email` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(100) NOT NULL,
+  PRIMARY KEY (`id_email`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+-- Listage des données de la table landingpage.email : ~4 rows (environ)
+INSERT INTO `email` (`id_email`, `email`) VALUES
+	(1, 'test@gmail.com'),
+	(2, 'test2@gmail.com'),
+	(3, 'gregher@mail.com'),
+	(4, 'email@email.fr'),
+	(5, 'gre@gre.fr');
+
 -- Listage de la structure de table landingpage. pricing
 CREATE TABLE IF NOT EXISTS `pricing` (
   `id_pricing` int NOT NULL AUTO_INCREMENT,
@@ -26,18 +41,19 @@ CREATE TABLE IF NOT EXISTS `pricing` (
   `price` float NOT NULL,
   `sale` int NOT NULL,
   `bandwidth` int NOT NULL,
-  `online_space` float NOT NULL DEFAULT '0',
+  `onlineSpace` float NOT NULL,
   `support` tinyint NOT NULL,
   `domain` varchar(50) NOT NULL,
-  `hidden_fees` tinyint NOT NULL DEFAULT '0',
+  `hiddenFees` tinyint NOT NULL,
+  `compteur` int NOT NULL DEFAULT '0',
   PRIMARY KEY (`id_pricing`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 -- Listage des données de la table landingpage.pricing : ~3 rows (environ)
-INSERT INTO `pricing` (`id_pricing`, `name`, `price`, `sale`, `bandwidth`, `online_space`, `support`, `domain`, `hidden_fees`) VALUES
-	(1, 'Starter', 9, 0, 1, 0.5, 0, '1', 0),
-	(2, 'Advanced', 19, 20, 2, 1, 1, '3', 0),
-	(3, 'Professional', 29, 0, 3, 2, 1, 'Unlimited', 0);
+INSERT INTO `pricing` (`id_pricing`, `name`, `price`, `sale`, `bandwidth`, `onlineSpace`, `support`, `domain`, `hiddenFees`, `compteur`) VALUES
+	(1, 'Started', 9, 0, 1, 0.5, 0, '1', 0, 91),
+	(2, 'Advanced', 19, 20, 2, 1, 1, '3', 0, 63),
+	(3, 'Professional', 29, 0, 3, 2, 1, 'Unlimited', 0, 19);
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
